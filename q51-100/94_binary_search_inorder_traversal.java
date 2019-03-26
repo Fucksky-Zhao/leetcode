377321f840f054fbccc20b265d9b4ef471830b16
Fucksky <zhaohao1997@zju.edu.cn>
2019-03-26 13:35:18 +0800

    q94 completed

---
+++ q51-100/94_binary_search_inorder_traversal.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public static List<Integer> list = new ArrayList<>();
+
+    //recursion
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        list.clear();
+        inOrder(root);
+        return list;
+    }
+
+    public void inOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inOrder(root.left);
+        list.add(root.val);
+        inOrder(root.right);
+    }
+
+
+    //iteration, using stack
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            list.add(curr.val);
+            curr = curr.right;
+        }
+        return list;
+    }
+
+}
