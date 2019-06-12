9d0cbbcbbbf9204c10cef90b66426b8c3bd00b08
Fucksky <zhaohao1997@zju.edu.cn>
2019-06-12 16:54:37 +0800

    q876 completed

---
+++ q851-900/q876_middle_of_the_linked_list.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode p = head;
+        ListNode q = head;
+        while (q.next != null && q.next.next != null) {
+            p = p.next;
+            q = q.next.next;
+        }
+        if (q.next == null) return p;
+        return p.next;
+    }
+}
