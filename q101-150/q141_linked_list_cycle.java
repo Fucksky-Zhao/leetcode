1abb0d2171c995ffdcfe77302fe9740aeb514aec
Fucksky <zhaohao1997@zju.edu.cn>
2019-06-12 13:58:47 +0800

    q141 completed

---
+++ q101-150/q141_linked_list_cycle.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode p = head;
+        ListNode q = head;
+        while(q.next != null && q.next.next != null) {
+            p = p.next;
+            q = q.next.next;
+            if (p == q) return true;
+        }
+        return false;
+    }
+}
