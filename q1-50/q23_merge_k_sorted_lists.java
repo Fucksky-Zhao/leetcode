9d42ae23baf0a59334721a335d2e022ed9772f6c
Fucksky <zhaohao1997@zju.edu.cn>
2019-06-13 13:11:38 +0800

    q23 completed

---
+++ q1-50/q23_merge_k_sorted_lists.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, (a, b) -> a.val - b.val);
+        ListNode head = new ListNode(-1);
+        ListNode p = head;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] != null) queue.add(lists[i]);
+        }
+        while (!queue.isEmpty()) {
+            p.next = queue.poll();
+            p = p.next;
+            if (p.next != null) queue.add(p.next);
+        }
+        return head.next;
+    }
+}
