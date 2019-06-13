5e644b6b63c519282304dcb70c74992237f77609
Fucksky <zhaohao1997@zju.edu.cn>
2019-06-13 17:38:41 +0800

    q101 completed

---
+++ q101-150/q101_symmetric_tree.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return helper(root.left, root.right);
+    }
+
+    private boolean helper(TreeNode a, TreeNode b) {
+        if (a == null && b == null) return true;
+        if (a == null || b == null) return false;
+        return a.val == b.val && helper(a.left, b.right) && helper(a.right, b.left);
+    }
+}
