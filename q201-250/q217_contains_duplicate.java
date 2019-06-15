b0b42d17fd95a30274a48c5eeb16a5133a8e05d2
Fucksky <zhaohao1997@zju.edu.cn>
2019-06-15 12:29:27 +0800

    q217 completed

---
+++ q201-250/q217_contains_duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if (nums == null || nums.length <= 1) return false;
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0 ; i < nums.length; i++) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
