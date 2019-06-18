7e0ae36193bf4775992d6baf682b4426a1a65d1f
Fucksky <zhaohao1997@zju.edu.cn>
2019-06-18 17:44:16 +0800

    q319 completed

---
+++ q301-350/q319_bulb_switcher.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        // int res = 0;
+        // for (int i = 1; i <= n; i++) {
+        //     int a = (int) Math.sqrt(i);
+        //     if (a * a == i) res++;
+        // }
+        // return res;
+        return (int) Math.sqrt(n);
+    }
+}
