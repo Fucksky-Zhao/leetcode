fe1c73d8d57f4394f21650e0bb540e5a2e6cd67d
Fucksky <zhaohao1997@zju.edu.cn>
2019-06-26 16:28:39 +0800

    q563 completed

---
+++ q551-600/q563_binary_tree_tilt.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int res = 0;
+
+    public int findTilt(TreeNode root) {
+        helper(root);
+        return res;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null) return 0;
+        int l = helper(root.left);
+        int r = helper(root.right);
+        res += Math.abs(l - r);
+        return l + r + root.val;
+    }
+}
